b651ea65998789bd20806e67aa5ff261b6776fde
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-06-10 20:42:52 +0700

    Update HomeFragment.java

--- 2540135441_Josesdio/app/src/main/java/com/example/bluejackpharmacy/HomeFragment.java
+++ 2540135441_Josesdio/app/src/main/java/com/example/bluejackpharmacy/HomeFragment.java
@@ -11,8 +11,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.bluejackpharmacy.database.Medicine;
 import com.example.bluejackpharmacy.databinding.FragmentHomeBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,21 +43,47 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        MedicineAdapter medicineAdapter = new MedicineAdapter();
-        binding.rvMedicine.setAdapter(medicineAdapter);
-        binding.rvMedicine.setLayoutManager(new LinearLayoutManager(requireContext()));
-
-        List<Medicine> medicineList = new ArrayList<>();
-        medicineList.add(new Medicine("Ibuprofen", "Rp12000", "PT Kimia Farma", "Ibuprofen adalah obat untuk untuk meredakan nyeri dan menurunkan deman. Obat ini juga memiliki efek antiradang. Ibuprofen bisa digunakan untuk meredakan nyeri haid, sakit kepala, sakit gigi, nyeri otot, atau nyeri sendi akibat radang sendi. ", R.drawable.ibuprofen));
-        medicineList.add(new Medicine("Betadine", "Rp6500", "PT Kalbe", "Betadine adalah produk antiseptik yang bermanfaat untuk mencegah pertumbuhan dan membunuh kuman penyebab infeksi.", R.drawable.betadine));
-        medicineList.add(new Medicine("Neurobion", "Rp18500", "PT Kimia Farma", "Neurobion adalah suplemen yang bermanfaat untuk mencegah dan mengobati gangguan saraf. Neurobion merupakan suplemen multivitamin yang mengandung vitamin B1, B6, dan B12 dalam dosis tinggi. ", R.drawable.neurobion));
-        medicineList.add(new Medicine("Sangobion", "Rp21500", "PT Kimia Farma", "Sangobion adalah suplemen untuk mengatasi kurang darah (anemia).", R.drawable.sangobion));
-        medicineList.add(new Medicine("Bisolvon", "Rp6500", "PT Kalbe", "Ini adalah bisolvon", R.drawable.bisolvon));
-        medicineList.add(new Medicine("Oralit", "Rp6500", "PT Kalbe", "Ini adalah oralit", R.drawable.oralit));
-        medicineList.add(new Medicine("Amoxcillin", "Rp6500", "PT Kalbe", "Ini adalah amoxcillin", R.drawable.amoxcillin));
-        medicineList.add(new Medicine("Paracetamol", "Rp6500", "PT Kalbe", "Ini adalah paracetamol", R.drawable.paracetamol));
-
-        medicineAdapter.setData(medicineList);
-
+        jsonParse();
     }
+
+    private void jsonParse() {
+        String url = "https://mocki.io/v1/ae13b04b-13df-4023-88a5-7346d5d3c7eb";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("medicines");
+                            List<Medicine> medicineList = new ArrayList<>();
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject medicine = jsonArray.getJSONObject(i);
+                                String name = medicine.getString("name");
+                                String price = medicine.getString("price");
+                                String manufacturer = medicine.getString("manufacturer");
+                                String description = medicine.getString("description");
+
+                                Medicine medicineObj = new Medicine(name, price, manufacturer, description, R.drawable.bisolvon);
+                                medicineList.add(medicineObj);
+                            }
+
+                            MedicineAdapter medicineAdapter = new MedicineAdapter();
+                            binding.rvMedicine.setAdapter(medicineAdapter);
+                            binding.rvMedicine.setLayoutManager(new LinearLayoutManager(requireContext()));
+                            medicineAdapter.setData(medicineList);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(requireContext());
+        requestQueue.add(request);
+    }
+
 }
