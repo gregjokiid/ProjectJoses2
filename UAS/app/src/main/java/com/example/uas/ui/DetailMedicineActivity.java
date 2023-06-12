6c6ca395d3cf5ba86ccf36008be76489466dfc4b
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-06-12 23:52:13 +0700

    Revert "create DetailMedicineActivity"
    This reverts commit 7a9ba53a61f3d6a2f19e8331f827133a9f42f628.

--- UAS/app/src/main/java/com/example/uas/ui/DetailMedicineActivity.java
+++ UAS/app/src/main/java/com/example/uas/ui/DetailMedicineActivity.java
@@ -1,16 +1,4 @@
 package com.example.uas.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-import com.example.uas.R;
-
-public class DetailMedicineActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_medicine);
-    }
+public class DetailMedicineActivity {
 }
