c452cdfa833f1cca87009cd809fbc544833430c7
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-06-12 23:26:26 +0700

    create model medicine

---
+++ UAS/app/src/main/java/com/example/uas/model/Medicine.java
@@ -0,0 +1,56 @@
+package com.example.uas.model;
+
+import java.io.Serializable;
+
+public class Medicine implements Serializable {
+    private String name, price, manufacturer, description;
+    private int image;
+
+    public Medicine(String name, String price, String manufacturer, String description, int image) {
+        this.name = name;
+        this.price = price;
+        this.manufacturer = manufacturer;
+        this.description = description;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
