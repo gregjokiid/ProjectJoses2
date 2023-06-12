6f92a4c4b64e649bf6cc9b1b389edd25240bec7f
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-06-12 09:57:01 +0700

    Revert "Update DBHelper.java"
    This reverts commit 79e2e26d1bfd15e4bb28ce6859cf9ea77dc9ca7a.

--- UAS/app/src/main/java/com/example/uas/DBHelper.java
+++ UAS/app/src/main/java/com/example/uas/DBHelper.java
@@ -17,15 +17,6 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String row_user_phone = "Phone";
     public static final String row_user_isVerified = "IsVerified";
 
-    // Tambahkan atribut untuk tabel medicines
-    public static String table_medicines = "medicines";
-    public static String row_medicine_id = "_id";
-    public static String row_medicine_name = "Name";
-    public static String row_medicine_manufacturer = "Manufacturer";
-    public static String row_medicine_price = "Price";
-    public static String row_medicine_image = "Image";
-    public static String row_medicine_description = "Description";
-
     private SQLiteDatabase db;
 
     public DBHelper(Context context) {
@@ -35,21 +26,14 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query_user = "CREATE TABLE " + table_user + "(" + row_user_id + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+        String query = "CREATE TABLE " + table_user + "(" + row_user_id + " INTEGER PRIMARY KEY AUTOINCREMENT,"
                 + row_user_name + " TEXT," + row_user_email + " TEXT," + row_user_password + " TEXT," + row_user_phone + " TEXT," + row_user_isVerified + " TEXT)";
-        db.execSQL(query_user);
-
-        // Tambahkan query untuk membuat tabel medicines
-        String query_medicines = "CREATE TABLE " + table_medicines + "(" + row_medicine_id + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + row_medicine_name + " TEXT," + row_medicine_manufacturer + " TEXT," + row_medicine_price + " TEXT," + row_medicine_image + " TEXT," + row_medicine_description + " TEXT)";
-        db.execSQL(query_medicines);
+        db.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + table_user);
-        db.execSQL("DROP TABLE IF EXISTS " + table_medicines);
-        onCreate(db);
     }
 
     //Insert Data
@@ -57,10 +41,6 @@ public class DBHelper extends SQLiteOpenHelper {
         db.insert(table_user, null, values);
     }
 
-    //Insert Medicine Data
-    public void insertMedicineData(ContentValues values){
-        db.insert(table_medicines, null, values);
-    }
 
     public String checkUser(String email, String password){
         String[] columns = {row_user_id, row_user_isVerified};
@@ -89,54 +69,5 @@ public class DBHelper extends SQLiteOpenHelper {
         } else {
             return "false";
         }
-    }
-
-    // Getter dan Setter untuk atribut medicines
-    public String getMedicineId() {
-        return row_medicine_id;
-    }
-
-    public String getMedicineName() {
-        return row_medicine_name;
-    }
-
-    public String getMedicineManufacturer() {
-        return row_medicine_manufacturer;
-    }
-
-    public String getMedicinePrice() {
-        return row_medicine_price;
-    }
-
-    public String getMedicineImage() {
-        return row_medicine_image;
-    }
-
-    public String getMedicineDescription() {
-        return row_medicine_description;
-    }
-
-    public void setMedicineId(String id) {
-        row_medicine_id = id;
-    }
-
-    public void setMedicineName(String name) {
-        row_medicine_name = name;
-    }
-
-    public void setMedicineManufacturer(String manufacturer) {
-        row_medicine_manufacturer = manufacturer;
-    }
-
-    public void setMedicinePrice(String price) {
-        row_medicine_price = price;
-    }
-
-    public void setMedicineImage(String image) {
-        row_medicine_image = image;
-    }
-
-    public void setMedicineDescription(String description) {
-        row_medicine_description = description;
     }
 }
