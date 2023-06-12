2fa0b1474db0b9321d2177e9810b1e1a3235f2ad
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-06-12 19:13:27 +0700

    Revert "create intent transfer data"
    This reverts commit 4297ad318b728e8a0b043f013a331dfd706e8050.

--- UAS/app/src/main/java/com/example/uas/ui/VerifyOTPActivity.java
+++ UAS/app/src/main/java/com/example/uas/ui/VerifyOTPActivity.java
@@ -90,7 +90,6 @@ public class VerifyOTPActivity extends AppCompatActivity {
                             progressBar.setVisibility(View.GONE);
                             buttonVerify.setVisibility(View.VISIBLE);
                             if(task.isSuccessful()){
-                                String email = getIntent().getStringExtra("email");
                                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                                 startActivity(intent);
