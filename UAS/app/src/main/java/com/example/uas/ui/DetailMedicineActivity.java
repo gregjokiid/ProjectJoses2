7a9ba53a61f3d6a2f19e8331f827133a9f42f628
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-06-12 23:37:24 +0700

    create DetailMedicineActivity

--- UAS/app/src/main/java/com/example/uas/ui/DetailMedicineActivity.java
+++ UAS/app/src/main/java/com/example/uas/ui/DetailMedicineActivity.java
@@ -1,4 +1,16 @@
 package com.example.uas.ui;
 
-public class DetailMedicineActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.uas.R;
+
+public class DetailMedicineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_medicine);
+    }
 }
