2ad41c6dbe01cd831a6cca3b9e33f82249cc471a
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-06-12 23:35:00 +0700

    create item_medicine & medicineadapter

---
+++ UAS/app/src/main/java/com/example/uas/adapter/MedicineAdapter.java
@@ -0,0 +1,73 @@
+package com.example.uas.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.uas.R;
+import com.example.uas.databinding.ItemMedicineBinding;
+import com.example.uas.model.Medicine;
+import com.example.uas.ui.DetailMedicineActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MedicineAdapter extends RecyclerView.Adapter<MedicineAdapter.ViewHolder> {
+
+    List<Medicine> listMedicine = new ArrayList<>();
+
+    void setData(List<Medicine> listMedicine) {
+        this.listMedicine.clear();
+        this.listMedicine.addAll(listMedicine);
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MedicineAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_medicine, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MedicineAdapter.ViewHolder holder, int position) {
+        Medicine medicine = listMedicine.get(position);
+        holder.bindData(medicine);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = holder.itemView.getContext();
+                Intent intent = new Intent(context, DetailMedicineActivity.class);
+                intent.putExtra("medicine", medicine);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return listMedicine.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ItemMedicineBinding binding;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = ItemMedicineBinding.bind(itemView);
+        }
+
+        public void bindData(Medicine data) {
+            binding.ivMedicine.setImageResource(data.getImage());
+            binding.tvName.setText(data.getName());
+            binding.tvManufacturer.setText(data.getManufacturer());
+            binding.tvPrice.setText(data.getPrice());
+        }
+    }
+}
