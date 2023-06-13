0f92f678e80957d6b24c6d21ab7f5ffd6227a60a
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-06-14 04:01:29 +0700

    create data dummy

--- UAS/app/src/main/java/com/example/uas/model/MedicineData.java
+++ UAS/app/src/main/java/com/example/uas/model/MedicineData.java
@@ -7,23 +7,43 @@ import java.util.ArrayList;
 public class MedicineData {
 
     private static final String[] name = {
-            //
+            "Ibuprofen",
+            "Betadine",
+            "Neurobion",
+            "Sangobion",
+            "Bisolvon"
     };
 
     private static final String[] manufacturer = {
-            //
+            "PT Kimia Farma",
+            "PT Kimia Farma",
+            "PT Kimia Farma",
+            "PT Kimia Farma",
+            "PT Kimia Farma"
     };
 
     private static final String[] price = {
-            //
+            "Rp12000",
+            "Rp12000",
+            "Rp12000",
+            "Rp12000",
+            "Rp12000"
     };
 
     private static final int[] image = {
-            //
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background
     };
 
     private static final String[] description = {
-            //
+            "Ini adalah oralit",
+            "Ini adalah oralit",
+            "Ini adalah oralit",
+            "Ini adalah oralit",
+            "Ini adalah oralit"
     };
 
     public static ArrayList<Medicine> getListData() {
