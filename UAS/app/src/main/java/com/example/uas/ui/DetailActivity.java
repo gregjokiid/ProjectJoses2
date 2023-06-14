7ee4f4d7b46ee1e5d3b93c2af7fd4595707e6cca
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-06-14 22:35:50 +0700

    Update DetailActivity.java

--- UAS/app/src/main/java/com/example/uas/ui/DetailActivity.java
+++ UAS/app/src/main/java/com/example/uas/ui/DetailActivity.java
@@ -3,13 +3,18 @@ package com.example.uas.ui;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -51,6 +56,29 @@ public class DetailActivity extends AppCompatActivity {
         tvMedicinePrice.setText(medicinePrice);
         tvMedicineDescription.setJustificationMode(Layout.JUSTIFICATION_MODE_INTER_WORD);
         tvMedicineDescription.setText(medicineDescription);
+
+        Button btnBeli = findViewById(R.id.btn_beli);
+        btnBeli.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+    }
+
+    public void openDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(DetailActivity.this);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_beli_obat, null);
+        AlertDialog dialog = builder.create();
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+
+            }
+        });
+        dialog.show();
     }
 
     @Override
