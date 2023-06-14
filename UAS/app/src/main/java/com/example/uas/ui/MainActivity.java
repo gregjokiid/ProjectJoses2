c8b8a56a2e80c053d2feb758db39ffb4d791dff8
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-06-14 13:24:41 +0700

    delete unused code

--- UAS/app/src/main/java/com/example/uas/ui/MainActivity.java
+++ UAS/app/src/main/java/com/example/uas/ui/MainActivity.java
@@ -1,16 +1,11 @@
 package com.example.uas.ui;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
-import android.app.SearchManager;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -28,9 +23,6 @@ import com.android.volley.toolbox.Volley;
 import com.example.uas.R;
 import com.example.uas.adapter.MedicineAdapter;
 import com.example.uas.model.Medicine;
-import com.example.uas.model.MedicineData;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
