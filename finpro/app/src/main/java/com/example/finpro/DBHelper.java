01ed41ebdd9b5ed9770034da49088f700205647d
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-06-15 17:37:30 +0700

    Update DBHelper.java

--- finpro/app/src/main/java/com/example/finpro/DBHelper.java
+++ finpro/app/src/main/java/com/example/finpro/DBHelper.java
@@ -102,7 +102,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
         if (count > 0) {
             if (isVerified.equals("0")) {
-                return "true";
+                return "otp";
             } else {
                 return "true";
             }
